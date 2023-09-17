b47e5ceae11696ad6be81dad0dcfa927eef998ce
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-09-17 08:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 
 function Enter(){
-	let head = document.querySelector(".heading");
-	head.innerText = "Entered Metaverse";
+	let head = document.querySelector("#status");
+	head.innerHTML = "<h1>Entered Metaverse</h1>";
 }
